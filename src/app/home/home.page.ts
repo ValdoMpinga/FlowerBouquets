a31ecb2cea0c8b54d747d2585569472227c2d955
Valdo Mpinga <valdompinga2@gmail.com>
2023-04-02 17:15:20 +0100

    feat: swiper library installed and configured.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  items = [
+    { title: 'Card 1', description: 'This is card 1' },
+    { title: 'Card 2', description: 'This is card 2' },
+    { title: 'Card 3', description: 'This is card 3' },
+    { title: 'Card 4', description: 'This is card 4' },
+    { title: 'Card 5', description: 'This is card 5' },
+    { title: 'Card 6', description: 'This is card 6' },
+    { title: 'Card 7', description: 'This is card 7' },
+    { title: 'Card 8', description: 'This is card 8' },
+    { title: 'Card 9', description: 'This is card 9' },
+    { title: 'Card 10', description: 'This is card 10' },
+  ];
 }
